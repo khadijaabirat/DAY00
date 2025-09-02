777aafe22671e0718852449005a9d73b782a0e14
khadijaabirat <abiratkhadija@gmail.com>
2025-09-02 14:20:31 +0100

    challange9

--- challange/challange9.c
+++ challange/challange9.c
@@ -9,9 +9,9 @@ int main() {
     printf("Entrez les trois coordonnées du deuxième pointe : ");
     scanf("%lf %lf %lf", &x2, &y2, &z2);
     printf("%lf", pow(8,6));
-   // d = sqrt(pow(x2 - x1,  2.0) + pow(y2 - y1, 2.0) + pow(z2 - z1, 2.0));
+    d = sqrt(pow(x2 - x1,  2.0) + pow(y2 - y1, 2.0) + pow(z2 - z1, 2.0));
 
-   // printf("La distance entre les deux points est : %.2lf\n", d);
+    printf("La distance entre les deux points est : %.2lf\n", d);
 
     return 0;
     }
