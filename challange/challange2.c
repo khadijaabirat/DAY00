22f01d8b1510831325a1b121ddfc0feec519b986
khadijaabirat <abiratkhadija@gmail.com>
2025-09-02 15:11:41 +0100

    challange 2

---
+++ challange/challange2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+
+    printf("Veuillez saisir un caractère : ");
+    scanf(" %c", &c);
+    switch (c) {
+        case 'a':
+                    printf("%c est une voyelle.\n", c);
+        case 'e':
+                    printf("%c est une voyelle.\n", c);
+        case 'i':
+                    printf("%c est une voyelle.\n", c);
+        case 'o':
+                    printf("%c est une voyelle.\n", c);
+        case 'u':
+    
+            printf("%c est une voyelle.\n", c);
+            break;
+
+        default:
+            printf("%c n'est pas une voyelle c'est une consonne.\n", c);
+            break; 
+    }
+    return 0;
+}
+    
