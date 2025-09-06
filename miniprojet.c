52cd4d2f07e043d677061284a35f77833e0baa93
khadijaabirat <abiratkhadija@gmail.com>
2025-09-06 14:10:42 +0200

    miniproject

--- miniprojet.c
+++ miniprojet.c
@@ -1,139 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#define maxchar 50
-
-int main() {
-    char titre[maxchar][maxchar];
-    char auteur[maxchar][maxchar];
-    float prix[maxchar];
-    int quantite[maxchar];
-    int i, n = 0, c, nquantite;
-    char nom[maxchar];
-    char sup[maxchar];
-    int trouve;
-
-    do {
-        printf("\n_______ MENU _______\n");
-        printf("1_ Ajouter un livre au stock.\n");
-        printf("2_ Afficher tous les livres disponibles.\n");
-        printf("3_ Rechercher un livre par son titre.\n");
-        printf("4_ Mettre a jour la quantite d'un livre.\n");
-        printf("5_ Supprimer un livre du stock.\n");
-        printf("6_ Afficher le nombre total de livres.\n");
-        printf("Choisissez une action: ");
-        scanf("%d", &c);
-        getchar();
-
-        switch (c) {
-        case 1:
-            printf("Titre: ");
-            fgets(titre[n], sizeof(titre[n]), stdin);
-            titre[n][strcspn(titre[n], "\n")] = '\0';
-
-            printf("Auteur: ");
-            fgets(auteur[n], sizeof(auteur[n]), stdin);
-            auteur[n][strcspn(auteur[n], "\n")] = '\0';
-
-            printf("Prix: ");
-            scanf("%f", &prix[n]);
-
-            printf("Quantite: ");
-            scanf("%d", &quantite[n]);
-
-            getchar();
-
-            n++;
-            break;
-
-        case 2:
-            if(n==0)
-        {
-            printf("Aucun livre disponible.\n");
-        }
-            for (i = 0; i < n; i++)
-                printf("\nLivre %d : %s , %s , %.2f , %d",
-                       i + 1, titre[i], auteur[i], prix[i], quantite[i]);
-            break;
-
-        case 3:
-            printf("qu il est le titre de livre que vous voullez chercher ?: ");
-            fgets(nom, sizeof(nom), stdin);
-            nom[strcspn(nom, "\n")] = '\0';
-
-            trouve = 0;
-            for (i = 0; i < n; i++)
-             {
-                if (strcmp(nom, titre[i]) == 0)
-                    {
-                    printf("Livre %d : %s , %s , %.2f , %d\n",
-                           i + 1, titre[i], auteur[i], prix[i], quantite[i]);
-                    trouve = 1;
-                    break;
-                   }
-            }
-            if (trouve==0)
-                printf("Ce titre n'existe pas: %s\n", nom);
-            break;
-
-        case 4:
-            printf("qu il est le titre de livre que vous voullez modiffier sa quantite ");
-            fgets(nom, sizeof(nom), stdin);
-            nom[strcspn(nom, "\n")] = '\0';
-
-            trouve = 0;
-            for (i = 0; i < n; i++) {
-                if (strcmp(nom, titre[i]) == 0) {
-                    printf("Nouvelle quantite: ");
-                    scanf("%d", &nquantite);
-                    getchar();
-                    quantite[i] = nquantite;
-                    trouve = 1;
-                    break;
-                }
-            }
-            if (trouve==0)
-                printf("Ce titre n'existe pas: %s\n", nom);
-            break;
-
-        case 5:
-            printf("qu il est le titre de livre que vous voullez supprimer en stock ");
-            fgets(sup, sizeof(sup), stdin);
-            sup[strcspn(sup, "\n")] = '\0';
-
-            trouve = 0;
-            for (i = 0; i < n; i++)
-                {
-                if (strcmp(sup, titre[i]) == 0)
-                 {
-                    for (int j = i; j < n -1; j++)
-                     {
-                        strcpy(titre[j], titre[j + 1]);
-                        strcpy(auteur[j], auteur[j + 1]);
-                        prix[j] = prix[j + 1];
-                        quantite[j] = quantite[j + 1];
-                    }
-                    n--;
-                    trouve = 1;
-                    break;
-                }
-            }
-            if (trouve==0)
-                printf("Ce titre n'existe pas: %s\n", sup);
-            break;
-
-        case 6:
-            printf("Nombre total de livres: %d\n", n);
-            break;
-
-        case 0:
-            printf("Au revoir!\n");
-            break;
-
-        default:
-            printf("%d n'est pas une option valide.\n", c);
-            break;
-        }
-    } while (c != 0);
-
-    return 0;
-}
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<CodeBlocks_project_file>
+	<FileVersion major="1" minor="6" />
+	<Project>
+		<Option title="miniprojectfonction" />
+		<Option execution_dir="C:/Users/DELL/Documents/DAY00/day2/tableaux/miniprojectfonction/" />
+		<Option pch_mode="2" />
+		<Option compiler="gcc" />
+		<Build>
+			<Target title="Debug">
+				<Option output="bin/Debug/miniprojectfonction" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Debug/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-g" />
+				</Compiler>
+			</Target>
+			<Target title="Release">
+				<Option output="bin/Release/miniprojectfonction" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Release/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-O2" />
+				</Compiler>
+				<Linker>
+					<Add option="-s" />
+				</Linker>
+			</Target>
+		</Build>
+		<Compiler>
+			<Add option="-Wall" />
+		</Compiler>
+		<Unit filename="miniprojectfonction/main.c">
+			<Option compilerVar="CC" />
+		</Unit>
+		<Unit filename="miniprojectfonction/miniprojectfonction.cbp" />
+		<Unit filename="miniprojetfonction.c">
+			<Option compilerVar="CC" />
+		</Unit>
+		<Extensions>
+			<lib_finder disable_auto="1" />
+		</Extensions>
+	</Project>
+</CodeBlocks_project_file>
--- miniprojetfonction.c
+++ miniprojetfonction.c
@@ -1,150 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#define maxchar 50
-
-char titre[maxchar][maxchar];
-char auteur[maxchar][maxchar];
-float prix[maxchar];
-int quantite[maxchar];
-
-void ajouterLivre(int *n)
-{
-    printf("Titre: ");
-    fgets(titre[*n], maxchar, stdin);
-    titre[*n][strcspn(titre[*n], "\n")] = '\0';
-
-    printf("Auteur: ");
-    fgets(auteur[*n], maxchar, stdin);
-    auteur[*n][strcspn(auteur[*n], "\n")] = '\0';
-
-    printf("Prix: ");
-    scanf("%f", &prix[*n]);
-
-    printf("Quantite: ");
-    scanf("%d", &quantite[*n]);
-
-    getchar();
-
-    (*n)++;
-}
-
-void afficherLivres(int n)
- {
-    if(n==0)
-        {
-            printf("Aucun livre disponible.\n");
-            return;
-        }
-    for(int i=0;i<n;i++)
-        printf("Livre %d : %s , %s , %.2f , %d\n", i+1, titre[i], auteur[i], prix[i], quantite[i]);
-}
-
-void rechercherLivre(int n)
-{
-    char nom[maxchar];
-    int trouve = 0;
-    printf("qu il est le titre de livre que vous voullez chercher ? ");
-    fgets(nom, maxchar, stdin);
-    nom[strcspn(nom, "\n")] = '\0';
-
-    for(int i=0;i<n;i++)
-    {
-        if(strcmp(nom, titre[i]) == 0) {
-            printf("Livre %d : %s , %s , %.2f , %d\n", i+1, titre[i], auteur[i], prix[i], quantite[i]);
-            trouve = 1;
-            break;
-        }
-    }
-    if(trouve==0)
-        printf("Ce titre n'existe pas: %s\n", nom);
-
-}
-
-void modifierQuantite(int n)
- {
-    char nom[maxchar];
-    int trouve = 0, nquantite;
-    printf("qu il est le titre de livre que vous voullez modiffier sa quantite");
-    fgets(nom, maxchar, stdin);
-    nom[strcspn(nom, "\n")] = '\0';
-
-    for(int i=0;i<n;i++)
-    {
-        if(strcmp(nom, titre[i]) == 0)
-        {
-            printf("Nouvelle quantite: ");
-            scanf("%d", &nquantite);
-            getchar();
-            quantite[i] = nquantite;
-            trouve = 1;
-            break;
-        }
-    }
-    if(trouve==0)
-     printf("Ce titre n'existe pas: %s\n", nom);
-}
-
-void supprimerLivre(int *n) {
-    char sup[maxchar];
-    int trouve = 0;
-    printf("qu il est le titre de livre que vous voullez supprimer en stock ");
-    fgets(sup, maxchar, stdin);
-    sup[strcspn(sup, "\n")] = '\0';
-
-    for(int i=0;i<*n;i++)
-    {
-        if(strcmp(sup, titre[i]) == 0)
-        {
-            for(int j=i;j<*n-1;j++)
-            {
-                strcpy(titre[j], titre[j+1]);
-                strcpy(auteur[j], auteur[j+1]);
-                prix[j] = prix[j+1];
-                quantite[j] = quantite[j+1];
-            }
-            (*n)--;
-            trouve = 1;
-            break;
-        }
-    }
-    if(trouve==0)
-     printf("Ce titre n'existe pas: %s\n", sup);
-}
-
-int main() {
-    int n = 0;
-    int c;
-
-    do {
-        printf("\n======= MENU =======\n");
-        printf("1 - Ajouter un livre\n");
-        printf("2 - Afficher tous les livres\n");
-        printf("3 - Rechercher un livre\n");
-        printf("4 - Modifier quantite\n");
-        printf("5 - Supprimer un livre\n");
-        printf("6 - Nombre total de livres\n");
-        printf("Choisissez une option: ");
-        scanf("%d", &c);
-        getchar();
-
-        switch(c)
-         {
-            case 1: ajouterLivre(&n);
-                    break;
-            case 2: afficherLivres(n);
-                    break;
-            case 3: rechercherLivre(n);
-                    break;
-            case 4: modifierQuantite(n);
-                    break;
-            case 5: supprimerLivre(&n);
-                    break;
-            case 6: printf("Nombre total de livres: %d\n", n);
-                    break;
-            default:printf("%d n'est pas une option valide.\n", c);
-                    break;
-        }
-    } while(c != 0);
-
-    return 0;
-}
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<CodeBlocks_project_file>
+	<FileVersion major="1" minor="6" />
+	<Project>
+		<Option title="miniprojectfonction" />
+		<Option execution_dir="C:/Users/DELL/Documents/DAY00/day2/tableaux/miniprojectfonction/" />
+		<Option pch_mode="2" />
+		<Option compiler="gcc" />
+		<Build>
+			<Target title="Debug">
+				<Option output="bin/Debug/miniprojectfonction" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Debug/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-g" />
+				</Compiler>
+			</Target>
+			<Target title="Release">
+				<Option output="bin/Release/miniprojectfonction" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Release/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-O2" />
+				</Compiler>
+				<Linker>
+					<Add option="-s" />
+				</Linker>
+			</Target>
+		</Build>
+		<Compiler>
+			<Add option="-Wall" />
+		</Compiler>
+		<Unit filename="miniprojectfonction/main.c">
+			<Option compilerVar="CC" />
+		</Unit>
+		<Unit filename="miniprojectfonction/miniprojectfonction.cbp" />
+		<Unit filename="miniprojetfonction.c">
+			<Option compilerVar="CC" />
+		</Unit>
+		<Extensions>
+			<lib_finder disable_auto="1" />
+		</Extensions>
+	</Project>
+</CodeBlocks_project_file>
