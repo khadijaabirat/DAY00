c4816cfab82577b2f149efda24ec750acd698a7a
khadijaabirat <abiratkhadija@gmail.com>
2025-09-08 15:46:12 +0100

    miniproject2.c

--- miniproject2.c
+++ miniproject2.c
@@ -1,117 +1,137 @@
 #include <stdio.h>
 #include <string.h>
+#define MAX 100  
 
-#define MAX 100  // تحديد حجم المصفوفة
-
-struct Contact {
-    char nom[50];
-    char telephone[20];
-    char email[50];
+struct Contact
+ {
+    char nom[MAX];
+    char telephone[MAX];
+    char email[MAX];
 };
 
 int nombreContacts = 0;
 struct Contact lescontacts[MAX];
 
-void ajouterContact() {
-    if(nombreContacts >= MAX) {
+void ajouterContact()
+ {
+    printf("Entrez le nom: ");
+    fgets(lescontacts[nombreContacts].nom, MAX, stdin);
+
+    printf("Entrez le numéro de téelephone: ");
+    fgets(lescontacts[nombreContacts].telephone, MAX, stdin);
+
+    printf("Entrez l'email: ");
+    fgets(lescontacts[nombreContacts].email, MAX, stdin);
+
+    getchar(); 
+
+    nombreContacts++;
+    
+    if(nombreContacts >= MAX) 
+    {
         printf("L'espace est plein !\n");
         return;
     }
-    printf("Entrez le nom: ");
-    scanf(" %49[^\n]", lescontacts[nombreContacts].nom);
-    printf("Entrez le numéro de téléphone: ");
-    scanf(" %19[^\n]", lescontacts[nombreContacts].telephone);
-    printf("Entrez l'email: ");
-    scanf(" %49[^\n]", lescontacts[nombreContacts].email);
-    nombreContacts++;
-    printf("Contact ajouté avec succès !\n");
 }
+void modifierContact()
+ {
+    char modiffiernom[MAX];
 
-void afficherContacts() {
-    if(nombreContacts == 0) {
-        printf("Aucun contact à afficher.\n");
-        return;
-    }
-    for(int i = 0; i < nombreContacts; i++) {
-        printf("\nContact %d:\n", i+1);
-        printf("Nom: %s\n", lescontacts[i].nom);
-        printf("Téléphone: %s\n", lescontacts[i].telephone);
-        printf("Email: %s\n", lescontacts[i].email);
-    }
-}
+    printf("Entrez le nom du contact que vous voullez modifier: ");
+    fgets(modiffiernom, MAX, stdin);
 
-void rechercherContact() {
-    char nom[50];
-    printf("Entrez le nom à rechercher: ");
-    scanf(" %49[^\n]", nom);
-    for(int i = 0; i < nombreContacts; i++) {
-        if(strcmp(lescontacts[i].nom, nom) == 0) {
-            printf("Contact trouvé:\n");
-            printf("Nom: %s\n", lescontacts[i].nom);
-            printf("Téléphone: %s\n", lescontacts[i].telephone);
-            printf("Email: %s\n", lescontacts[i].email);
-            return;
-        }
-    }
-    printf("Contact non trouvé.\n");
-}
-
-void modifierContact() {
-    char nom[50];
-    printf("Entrez le nom du contact à modifier: ");
-    scanf(" %49[^\n]", nom);
-    for(int i = 0; i < nombreContacts; i++) {
-        if(strcmp(lescontacts[i].nom, nom) == 0) {
-            printf("Entrez le nouveau numéro de téléphone: ");
-            scanf(" %19[^\n]", lescontacts[i].telephone);
+    for(int i = 0; i < nombreContacts; i++)
+    {
+        if(strcmp(lescontacts[i].nom, modiffiernom) == 0) 
+        {
+            printf("Entrez le nouveau numero de telephone: ");
+            scanf(" %s", lescontacts[i].telephone);
             printf("Entrez le nouvel email: ");
-            scanf(" %49[^\n]", lescontacts[i].email);
-            printf("Contact modifié avec succès !\n");
+            scanf(" %s", lescontacts[i].email);
             return;
         }
     }
-    printf("Contact non trouvé.\n");
+    printf("ce contact ne trouve pas");
 }
-
-void supprimerContact() {
-    char nom[50];
-    printf("Entrez le nom du contact à supprimer: ");
-    scanf(" %49[^\n]", nom);
-    for(int i = 0; i < nombreContacts; i++) {
-        if(strcmp(lescontacts[i].nom, nom) == 0) {
-            for(int j = i; j < nombreContacts-1; j++) {
+void supprimerContact()
+ {
+    char recherchenom[MAX];
+    printf("Entrez le nom du contact que vous voullez supprimer: ");
+    scanf(" %s",recherchenom);
+    for(int i = 0; i < nombreContacts; i++) 
+    {
+        if(strcmp(lescontacts[i].nom, recherchenom) == 0) 
+        {
+            for(int j = i; j < nombreContacts-1; j++) 
+            {
                 lescontacts[j] = lescontacts[j+1];
             }
             nombreContacts--;
-            printf("Contact supprimé avec succès !\n");
+            return;
+        }
+    }
+    printf("ce contact ne trouve pas.");
+}
+
+void afficherContacts()
+ {
+    for(int i = 0; i < nombreContacts; i++)
+    {
+        printf("le contact %d:\n son nom est: %s \n son telephone est: %s \n son email est: %s\n", i+1,lescontacts[i].nom,lescontacts[i].telephone,lescontacts[i].email);
+    }
+    if(nombreContacts == 0) 
+    {
+        printf("pas de contact");
+        return;
+    }
+}
+
+void rechercherContact() 
+{
+    char nom[MAX];
+    printf("Entrez le nom de contact que vou voullez rechercher: ");
+    scanf("%s", nom);
+    for(int i = 0; i < nombreContacts; i++)
+     {
+        if(strcmp(lescontacts[i].nom, nom) == 0) 
+        {
+            printf("le Contact est:\n Nom: %s\n Telephone: %s\n Email: %s\n",lescontacts[i].nom,lescontacts[i].telephone,lescontacts[i].email);
             return;
         }
     }
     printf("Contact non trouvé.\n");
 }
 
-int main() {
+
+
+
+int main() 
+{
     int choix;
     do {
-        printf("\n=== Gestion de Contacts ===\n");
+        printf("---- Gestion de Contacts ----\n");
         printf("1. Ajouter un contact\n");
         printf("2. Afficher tous les contacts\n");
         printf("3. Rechercher un contact\n");
         printf("4. Modifier un contact\n");
         printf("5. Supprimer un contact\n");
-        printf("0. Quitter\n");
-        printf("Choix: ");
+        printf("Choisissez une option:");
         scanf("%d", &choix);
-        while(getchar() != '\n'); // nettoyer le buffer
-
-        switch(choix) {
-            case 1: ajouterContact(); break;
-            case 2: afficherContacts(); break;
-            case 3: rechercherContact(); break;
-            case 4: modifierContact(); break;
-            case 5: supprimerContact(); break;
-            case 0: printf("Au revoir !\n"); break;
-            default: printf("Choix invalide !\n"); break;
+        getchar();
+        switch(choix) 
+        {
+            case 1: ajouterContact();
+            break;
+            case 2: afficherContacts(); 
+            break;
+            case 3: rechercherContact(); 
+            break;
+            case 4: modifierContact(); 
+            break;
+            case 5: supprimerContact(); 
+            break;
+            default: printf("ton choix invalide !\n"); 
+            break;
         }
     } while(choix != 0);
 
